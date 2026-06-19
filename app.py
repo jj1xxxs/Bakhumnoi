351e2ed5fde642406ffd659db5d47fd1e23740ba
Bakhumnoi <jangjann.pp@gmail.com>
2026-06-19 12:28:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from deep_translator import GoogleTranslator
+from langdetect import detect
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_text = event.message.text
     
     try:
-        # ตรวจสอบว่าเป็นภาษาอะไร
         lang = detect(user_text)
         
         if lang == 'th':
-            # ถ้าเป็นภาษาไทย ให้แปลเป็นรัสเซีย
             translated = GoogleTranslator(source='th', target='ru').translate(user_text)
             reply_text = translated
             
         elif lang == 'ru':
-            # ถ้าเป็นภาษารัสเซีย ให้แปลเป็นไทย
             translated = GoogleTranslator(source='ru', target='th').translate(user_text)
             reply_text = translated
             
         else:
-            # ถ้าเป็นภาษาอื่นๆ (เช่น อังกฤษ) ให้แจ้งเตือน
             reply_text = "กรุณาส่งข้อความภาษาไทยหรือรัสเซียครับ"
             
     except Exception as e:
-        # กรณีตรวจจับภาษาไม่ได้ (เช่น ส่งสติ๊กเกอร์ หรือพิมพ์อีโมจิมาอย่างเดียว)
         reply_text = "ขออภัยครับ ไม่สามารถแปลข้อความนี้ได้"
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_text)
-    )
-        else:
-            reply_text = "กรุณาส่งข้อความภาษาไทยครับ ผมจะแปลเป็นภาษารัสเซียให้"
-            
-    except Exception as e:
-        reply_text = "ขออภัยครับ เกิดข้อผิดพลาดในการแปล"
 
     line_bot_api.reply_message(
         event.reply_token,
